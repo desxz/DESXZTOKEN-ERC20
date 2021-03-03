4bc3aa471189a22052a7bc16fce4be6234acbcc7
desxz <muratgun545@gmail.com>
2021-03-03 23:18:21 +0300

    Token Value Edit

--- token.sol
+++ token.sol
@@ -5,6 +5,6 @@ import 'github/OpenZeppelin/openzeppelin-contracts/contracts/token/ERC20/ERC20.s
 contract Token is ERC20 {
 
     constructor () ERC20("DESXZTOKEN", "DXZ") {
-        _mint(msg.sender, 1 * (10 ** (uint256(decimals())/3)));
+        _mint(msg.sender, 1000 * (10 ** (uint256(decimals()))));
     }
 }
